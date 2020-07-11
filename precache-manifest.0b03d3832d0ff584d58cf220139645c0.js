cf3b01fe38e039f83a945dd4d1dc923f18ac13ad
ayub6717 <ayub15-6717@diu.edu.bd>
2020-07-11 20:17:59 +0600

    Updates

---
+++ precache-manifest.0b03d3832d0ff584d58cf220139645c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86f59e5da5e1c33751b8e2aca3fc8ad",
+    "url": "/react-modal-login/index.html"
+  },
+  {
+    "revision": "3a9cee8ea632c6724275",
+    "url": "/react-modal-login/static/css/main.a7adab5e.chunk.css"
+  },
+  {
+    "revision": "621d982b033d2b2c3625",
+    "url": "/react-modal-login/static/js/2.db005237.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/react-modal-login/static/js/2.db005237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9cee8ea632c6724275",
+    "url": "/react-modal-login/static/js/main.5c96ed80.chunk.js"
+  },
+  {
+    "revision": "5a2806a71ca5c93e2626",
+    "url": "/react-modal-login/static/js/runtime-main.ad87b895.js"
+  },
+  {
+    "revision": "4a6920d9d6bfdc3887f58a2c71b1b27f",
+    "url": "/react-modal-login/static/media/bg-6.4a6920d9.jpg"
+  }
+]);
